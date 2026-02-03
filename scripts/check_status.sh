4e294b4bdd7ff1c2ffecd30a7b85a17206345787
Angel <alfredocervantesgil@gmail.com>
2026-02-03 17:06:11 -0600

    Se agrego a scripts el script check_status.sh, se comprobo que funciona de una manera correcta y limpia

---
+++ scripts/check_status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo ""
+echo "-------------------------------------------------------"
+echo "       Angel Distro Mageia MSG Bienvenida - LINUX "
+echo "-------------------------------------------------------"
+echo ""
+
+echo "Nombre del equipo: "
+hostname
+echo ""
+
+echo "IP actual: "
+ip -4 addr show | awk '/inet /{print " - " $2 " (" $NF ")"}'
+echo ""
+
+echo "Espacio en disco: "
+df -h / | awk 'NR==1{print} NR==2{print}'
+
+echo "________________________________________________________"
+echo  ""
