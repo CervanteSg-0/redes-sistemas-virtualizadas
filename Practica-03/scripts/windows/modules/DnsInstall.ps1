38d438f589de1b58d717a328aefbdd1358ef48e9
Angel <alfredocervantesgil@gmail.com>
2026-02-20 17:00:00 -0700

    update file

---
+++ Practica-03/scripts/windows/modules/DnsInstall.ps1
@@ -0,0 +1,15 @@
+# windows/modules/DnsInstall.ps1
+
+. "$PSScriptRoot\Common.ps1"
+
+function Install-DnsRole {
+    Write-Host "== Instalando DNS Server (Windows Server) =="
+    $feat = Get-WindowsFeature DNS
+    if ($feat.Installed) {
+        Write-Host "[OK] DNS ya está instalado."
+        return
+    }
+    
+    Install-WindowsFeature DNS -IncludeManagementTools | Out-Null
+    Write-Host "[OK] DNS instalado."
+}
