63a200a1e1996eab3a6f82091c4a988140c36c78
Angel <alfredocervantesgil@gmail.com>
2026-02-20 17:30:28 -0700

    update file

--- Practica-03/scripts/windows/modules/DnsZone.ps1
+++ Practica-03/scripts/windows/modules/DnsZone.ps1
@@ -4,21 +4,23 @@
 
 function Configure-DnsZone {
     Write-Host "== Configurar Zona DNS (Windows Server) =="
-    $domain = Read-Host "Dominio :"
+    $domain = Read-Host "Dominio: "
     $clientIp = Read-Host "IP del CLIENTE a la que apuntara el dominio"
-    
+    $ttl = Read-Host "TTL (segundos)"
+
     # Validación para TTL
-    $ttl = Read-Host "TTL (segundos) "
     while ($ttl -lt 30 -or $ttl -gt 200000000) {
         Write-Host "[ERROR] "
-        $ttl = Read-Host "TTL (segundos) 
+        $ttl = Read-Host "TTL (segundos): "
     }
 
     # Configura la zona
     Add-DnsServerPrimaryZone -Name $domain -ZoneFile "$domain.dns" -DynamicUpdate NonsecureAndSecure | Out-Null
 
-    # Agregar registros A
+    # Agregar registros A (IP)
     Add-DnsServerResourceRecordA -ZoneName $domain -Name "@" -IPv4Address $clientIp -TimeToLive ([TimeSpan]::FromSeconds($ttl)) | Out-Null
+
+    # Agregar registros CNAME (www)
     Add-DnsServerResourceRecordCName -ZoneName $domain -Name "www" -HostNameAlias "$domain." -TimeToLive ([TimeSpan]::FromSeconds($ttl)) | Out-Null
 
     Write-Host "[OK] Zona y registros configurados: $domain"
